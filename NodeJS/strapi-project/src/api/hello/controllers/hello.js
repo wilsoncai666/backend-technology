ec59ce67a389b46c16cb7804e384701ade9bf5ee
WilsonCai <wilsoncai666@163.com>
2022-04-07 11:50:02 +0800

    V0.0.40 add console.log in hello api

--- NodeJS/strapi-project/src/api/hello/controllers/hello.js
+++ NodeJS/strapi-project/src/api/hello/controllers/hello.js
@@ -1,5 +1,6 @@
 module.exports = {
-  async index(ctx, next) { // called by GET /hello 
+  async index(ctx, next) { // called by GET /hello
+    console.log("----hello");
     ctx.body = 'Hello World!'; // we could also send a JSON
   },
 };
