d01c48e4111263d9ec3383e62d7a8a9f491c4a08
WilsonCai <wilsoncai666@163.com>
2022-04-11 17:34:52 +0800

    V0.0.44 add another api

--- NodeJS/strapi-project/src/api/machinelog/controllers/machinelog.js
+++ NodeJS/strapi-project/src/api/machinelog/controllers/machinelog.js
@@ -7,5 +7,10 @@ module.exports = {
     // console.log("--ld1055daily");
     const result = await k8pg('ld1055_dailyinfos').select();
     ctx.body = result; // we could also send a JSON
+  },
+  async ld1055Hydpumppresdis(ctx, next) { // called by GET /ld1055Daily
+    // console.log("--ld1055daily");
+    const result = await k8pg('ld1055_hydpumppresdis').select();
+    ctx.body = result; // we could also send a JSON
   }
 };
--- NodeJS/strapi-project/src/api/machinelog/routes/machinelog.js
+++ NodeJS/strapi-project/src/api/machinelog/routes/machinelog.js
@@ -4,6 +4,11 @@ module.exports = {
         method: 'GET',
         path: '/ld1055Daily',
         handler: 'machinelog.ld1055Daily',
+      },
+      {
+        method: 'GET',
+        path: '/ld1055Hydpumppresdis',
+        handler: 'machinelog.ld1055Hydpumppresdis',
       }
     ]
   }
